2b350337f27878b92cf8f170652dc8e524a1ddb5
two_monkey <two_monkey@bobodeMacBook-Pro.local>
2015-09-18 13:19:47 +0800

    Initial Commit

---
+++ SHFisher/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SHFisher
+//
+//  Created by two_monkey on 15/9/18.
+//  Copyright (c) 2015年 twomonkeys. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
